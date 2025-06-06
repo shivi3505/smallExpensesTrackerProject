11ad98c1ec29e1b0408a589d7267c0dcefaf4492
shivi3505 <jaiswalshivani432@gamil.com>
2025-06-06 19:21:38 +0530

    add js file

---
+++ index.js
@@ -0,0 +1,9 @@
+function patientDetails(event) {
+    event.preventDefault();
+    console.log(event.target.p_name.value)
+    console.log(event.target.p_email.value)
+    console.log(event.target.p_number.value)
+    console.log(event.target.p_date.value)
+    console.log(event.target.p_time.value);
+
+}
