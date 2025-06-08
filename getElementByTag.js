c7550af19f15a4e60ccb988c2bd625a5529f4916
shivi3505 <jaiswalshivani432@gamil.com>
2025-06-08 21:18:40 +0530

    add getElementByTag.js file

---
+++ getElementByTag.js
@@ -0,0 +1,6 @@
+// Write your code below:
+const fruit = document.getElementsByTagName('li');
+fruit[4].style.color = "blue";
+for (let i = 0; i < fruit.length; i++){
+    fruit[i].style.fontStyle = "italic";
+}
